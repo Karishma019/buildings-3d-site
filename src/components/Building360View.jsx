c7afa1b2e98ae770785646b2d21bae8862264717
Karishma019 <karishmaacharya63@gmail.com>
2024-10-14 18:20:58 +0530

    Update Building360View.jsx

--- src/components/Building360View.jsx
+++ src/components/Building360View.jsx
@@ -4,8 +4,8 @@ import BuildingView from "../models/BuildingView";
 
 const Building360View = () => {
   return (
-    <div className="h-screen flex justify-center items-center w-screen">
-      <div className="w-[900px] h-3/4">
+    <div className="h-screen flex justify-center items-center absolute top-0 -z-10 w-screen ">
+      <div className="w-full h-full">
         <Canvas camera={{ position: [0, 0, 2], fov: 60 }}>
           <BuildingView />
         </Canvas>
