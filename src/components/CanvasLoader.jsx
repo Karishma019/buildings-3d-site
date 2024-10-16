d3b829d138bc6910f7267c557dbf4d9e7551bdb6
Karishma019 <karishmaacharya63@gmail.com>
2024-10-16 16:59:40 +0530

    fixed loader position

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -4,14 +4,12 @@ import React from "react";
 const CanvasLoader = () => {
   return (
     <Html>
-      <section className="h-screen flex justify-center items-center fade-in-out">
-        <h1 className="uppercase text-4xl text-center pt-10">
-          <span className="bg-primary text-white text-3xl py-2 tracking-widest">
-            Estate
-          </span>
-          <span className="font-bold text-primary">Explorer</span>
-        </h1>
-      </section>
+      <h1 className="uppercase absolute -left-32 text-4xl">
+        <span className="bg-primary text-white text-3xl py-2 tracking-widest">
+          Estate
+        </span>
+        <span className="font-bold text-primary">Explorer</span>
+      </h1>
     </Html>
   );
 };
