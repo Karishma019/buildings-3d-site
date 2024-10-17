a037faea51944a5e2b22fada2de98c42a19ce252
Karishma019 <karishmaacharya63@gmail.com>
2024-10-17 23:09:15 +0530

    added copy functionality

--- src/components/MainViewSite.jsx
+++ src/components/MainViewSite.jsx
@@ -1,12 +1,29 @@
+import { useCallback, useState } from "react";
 import Header from "../components/Header";
 import bgVideo from "../img/bgMain.mp4";
 import { TiLocationArrowOutline } from "react-icons/ti";
+import { toast } from "react-toastify";
 
 const MainViewSite = (props) => {
+  const [showMessage, setShowMessage] = useState(false);
+  const currentUrl = window.location.href;
+
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard
+      .writeText(currentUrl)
+      .then(() => {
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 1000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  }, []);
+
   return (
-    <section className="h-screen"
-    ref={(el) => props.storeInputRef(el,0)}
-    >
+    <section className="h-screen" ref={(el) => props.storeInputRef(el, 0)}>
       <video
         className="absolute inset-0 w-full h-full object-cover"
         src={bgVideo}
@@ -22,9 +39,12 @@ const MainViewSite = (props) => {
         <p className="font-normal text-lg md:text-2xl">
           3 BHK PEACEFUL LIVINGS & SHOPS
         </p>
-        <button className="bg-neutral_200 bg-opacity-30 border rounded text-sm flex items-center px-3 py-1 mt-4">
+        <button
+          className="bg-neutral_200 bg-opacity-30 border rounded text-sm flex items-center px-3 py-1 mt-4"
+          onClick={copyToClipboard}
+        >
           <TiLocationArrowOutline className="text-xl" />
-          Share
+          {showMessage ? "Copied!!" : "Share"}
         </button>
       </div>
     </section>
