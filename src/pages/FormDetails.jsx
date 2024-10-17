3b35734234c1e57e5a6ceafba37cf147125d3739
Karishma019 <karishmaacharya63@gmail.com>
2024-10-18 00:31:26 +0530

    added full screen sample tour

--- src/pages/FormDetails.jsx
+++ src/pages/FormDetails.jsx
@@ -1,6 +1,7 @@
 import ReCAPTCHA from "react-google-recaptcha";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Header from "../components/Header";
 
 const FormDetails = () => {
   const [verified, setVerified] = useState(false);
@@ -68,185 +69,193 @@ const FormDetails = () => {
   }, []);
 
   return (
-    <section className="relative mt-10">
-      <div className="clipPath flex bg-gradient-to-t from-primary_100 -z-10 via-primary_50 to-white absolute h-full w-full"></div>
+    <>
+      {" "}
+      <Header />
+      <section className="relative mt-20">
+        <div className="clipPath flex bg-gradient-to-t from-primary_100 -z-10 via-primary_50 to-white absolute h-full w-full"></div>
 
-      <div className="text-center px-8">
-        <h1 className="lg:text-[40px] md:text-[32px] text-[24px] font-semibold">
-          Let Us Bring Your Vision to Life
-        </h1>
-        <p className="lg:text-2xl md:text-xl text-sm">
-          Simply fill out the form to get AR App for your project
-        </p>
-      </div>
-      <div className="w-full flex justify-center my-5 px-8">
-        <form
-          className="bg-white lg:w-2/3 w-full rounded-lg shadow-xl p-8"
-          onSubmit={handleSubmit}
-        >
-          <p className="lg:text-lg text-sm font-semibold">Your Details</p>
-          <div className="flex md:flex-row flex-col gap-5 my-5">
-            <label
-              htmlFor="fullName"
-              className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
-            >
-              Full Name
-              <input
-                type="text"
-                name="fullName"
-                placeholder="Enter your name"
-                id="fullName"
-                className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
-                onChange={handleChange}
-              />
-              {errors && (
-                <p className="text-xs text-red-500">{errors?.fullName}</p>
-              )}
-            </label>
-            <label
-              htmlFor="phoneNumber"
-              className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
-            >
-              Phone Number
-              <input
-                type="text"
-                name="phoneNumber"
-                placeholder="Enter your number"
-                id="phoneNumber"
-                className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
-                onChange={handleChange}
-              />
-              {errors && (
-                <p className=" text-xs text-red-500">{errors?.phoneNumber}</p>
-              )}
-            </label>
-            <label
-              htmlFor="email"
-              className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
-            >
-              Email Address
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter email address"
-                id="email"
-                className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
-                onChange={handleChange}
-              />
-              {errors && (
-                <p className=" text-xs text-red-500">{errors?.email}</p>
-              )}
-            </label>
-          </div>
-          <p className="lg:text-lg text-sm font-semibold">Project Details</p>
-          <div className="flex md:flex-row flex-col gap-5 my-5">
-            <label
-              htmlFor="projectName"
-              className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/2 w-full"
-            >
-              Project Name
-              <input
-                type="text"
-                name="projectName"
-                placeholder="Enter Project name"
-                id="projectName"
-                className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
-                onChange={handleChange}
-              />
-              {errors && (
-                <p className="w-1/2 text-xs text-red-500">
-                  {errors?.projectName}
-                </p>
-              )}
-            </label>
-            <label
-              htmlFor="projectType"
-              className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/2 w-full"
-            >
-              Project Type
-              <input
-                type="text"
-                name="projectType"
-                placeholder="Select Project Type"
-                id="projectType"
-                className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
-                onChange={handleChange}
-              />
-              {errors && (
-                <p className="w-1/2 text-xs text-red-500">
-                  {errors?.projectType}
-                </p>
-              )}
-            </label>
-          </div>
-          <div className="flex flex-col gap-2 my-5">
-            <p className="lg:text-sm text-[12px] w-full">Project Address</p>
-            <div className="flex md:flex-row flex-col gap-5">
-              <div className="md:w-1/3 w-full">
+        <div className="text-center px-8">
+          <h1 className="lg:text-[40px] md:text-[32px] text-[24px] font-semibold">
+            Let Us Bring Your Vision to Life
+          </h1>
+          <p className="lg:text-2xl md:text-xl text-sm">
+            Simply fill out the form to get AR App for your project
+          </p>
+        </div>
+        <div className="w-full flex justify-center my-5 px-8">
+          <form
+            className="bg-white lg:w-2/3 w-full rounded-lg shadow-xl p-8"
+            onSubmit={handleSubmit}
+          >
+            <p className="lg:text-lg text-sm font-semibold">Your Details</p>
+            <div className="flex md:flex-row flex-col gap-5 my-5">
+              <label
+                htmlFor="fullName"
+                className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
+              >
+                Full Name
                 <input
                   type="text"
-                  name="street"
-                  placeholder="Enter Street"
-                  id="street"
-                  className="outline-none border-b border-b-2  w-full border-neutral_400 lg:text-lg text-sm"
+                  name="fullName"
+                  placeholder="Enter your name"
+                  id="fullName"
+                  className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
                   onChange={handleChange}
                 />
                 {errors && (
-                  <p className="text-xs text-red-500 mt-2">{errors?.street}</p>
+                  <p className="text-xs text-red-500">{errors?.fullName}</p>
                 )}
-              </div>
-              <div className="md:w-2/3 w-full flex gap-5">
-                <div className="w-1/2">
+              </label>
+              <label
+                htmlFor="phoneNumber"
+                className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
+              >
+                Phone Number
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Enter your number"
+                  id="phoneNumber"
+                  className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
+                  onChange={handleChange}
+                />
+                {errors && (
+                  <p className=" text-xs text-red-500">{errors?.phoneNumber}</p>
+                )}
+              </label>
+              <label
+                htmlFor="email"
+                className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/3 w-full"
+              >
+                Email Address
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter email address"
+                  id="email"
+                  className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
+                  onChange={handleChange}
+                />
+                {errors && (
+                  <p className=" text-xs text-red-500">{errors?.email}</p>
+                )}
+              </label>
+            </div>
+            <p className="lg:text-lg text-sm font-semibold">Project Details</p>
+            <div className="flex md:flex-row flex-col gap-5 my-5">
+              <label
+                htmlFor="projectName"
+                className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/2 w-full"
+              >
+                Project Name
+                <input
+                  type="text"
+                  name="projectName"
+                  placeholder="Enter Project name"
+                  id="projectName"
+                  className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
+                  onChange={handleChange}
+                />
+                {errors && (
+                  <p className="w-1/2 text-xs text-red-500">
+                    {errors?.projectName}
+                  </p>
+                )}
+              </label>
+              <label
+                htmlFor="projectType"
+                className="flex flex-col gap-2 lg:text-sm text-[12px] md:w-1/2 w-full"
+              >
+                Project Type
+                <input
+                  type="text"
+                  name="projectType"
+                  placeholder="Select Project Type"
+                  id="projectType"
+                  className="outline-none border-b border-b-2 border-neutral_400 lg:text-lg text-sm"
+                  onChange={handleChange}
+                />
+                {errors && (
+                  <p className="w-1/2 text-xs text-red-500">
+                    {errors?.projectType}
+                  </p>
+                )}
+              </label>
+            </div>
+            <div className="flex flex-col gap-2 my-5">
+              <p className="lg:text-sm text-[12px] w-full">Project Address</p>
+              <div className="flex md:flex-row flex-col gap-5">
+                <div className="md:w-1/3 w-full">
                   <input
                     type="text"
-                    name="city"
-                    placeholder="Enter City"
-                    id="city"
-                    className="outline-none border-b border-b-2 border-neutral_400 w-full lg:text-lg text-sm"
+                    name="street"
+                    placeholder="Enter Street"
+                    id="street"
+                    className="outline-none border-b border-b-2  w-full border-neutral_400 lg:text-lg text-sm"
                     onChange={handleChange}
                   />
                   {errors && (
-                    <p className=" text-xs text-red-500 mt-2">{errors?.city}</p>
-                  )}
-                </div>
-                <div className="w-1/2">
-                  <input
-                    type="text"
-                    name="state"
-                    placeholder="Enter State"
-                    id="state"
-                    className="outline-none border-b border-b-2 border-neutral_400  lg:text-lg text-sm"
-                    onChange={handleChange}
-                  />
-                  {errors && (
-                    <p className=" text-xs mt-2 text-red-500">
-                      {errors?.state}
+                    <p className="text-xs text-red-500 mt-2">
+                      {errors?.street}
                     </p>
                   )}
                 </div>
+                <div className="md:w-2/3 w-full flex gap-5">
+                  <div className="w-1/2">
+                    <input
+                      type="text"
+                      name="city"
+                      placeholder="Enter City"
+                      id="city"
+                      className="outline-none border-b border-b-2 border-neutral_400 w-full lg:text-lg text-sm"
+                      onChange={handleChange}
+                    />
+                    {errors && (
+                      <p className=" text-xs text-red-500 mt-2">
+                        {errors?.city}
+                      </p>
+                    )}
+                  </div>
+                  <div className="w-1/2">
+                    <input
+                      type="text"
+                      name="state"
+                      placeholder="Enter State"
+                      id="state"
+                      className="outline-none border-b border-b-2 border-neutral_400  lg:text-lg text-sm"
+                      onChange={handleChange}
+                    />
+                    {errors && (
+                      <p className=" text-xs mt-2 text-red-500">
+                        {errors?.state}
+                      </p>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <p className="lg:text-lg text-sm font-semibold mt-10">Payment</p>
-          <div className="bg-neutral_100 h-56 mt-4 rounded"></div>
-          {showCaptcha && (
-            <div className="mt-5">
-              <ReCAPTCHA
-                sitekey="6LcWGmIqAAAAAAlypp4Qzszma3Vctbw2pBfVHL2b" // Replace with your site key
-                onChange={onChange}
-              />
-            </div>
-          )}
+            <p className="lg:text-lg text-sm font-semibold mt-10">Payment</p>
+            <div className="bg-neutral_100 h-56 mt-4 rounded"></div>
+            {showCaptcha && (
+              <div className="mt-5">
+                <ReCAPTCHA
+                  sitekey="6LcWGmIqAAAAAAlypp4Qzszma3Vctbw2pBfVHL2b" // Replace with your site key
+                  onChange={onChange}
+                />
+              </div>
+            )}
 
-          <button
-            type="submit"
-            className="bg-primary_500 text-neutral_0 rounded-lg px-4 py-2 mt-5"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </section>
+            <button
+              type="submit"
+              className="bg-primary_500 text-neutral_0 rounded-lg px-4 py-2 mt-5"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
