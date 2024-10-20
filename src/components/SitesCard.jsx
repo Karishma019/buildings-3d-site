b188106e43e2a00102a9c49136d535e9b7bf1f71
Yash Khandhediya <yashhkhandhediya@gmail.com>
2024-10-20 08:30:29 +0530

    Added 111 and 64 image for home page portion

--- src/components/SitesCard.jsx
+++ src/components/SitesCard.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
-import site1Img from "../img/card.jpeg";
+import site111Img from "../img/111_card.jpeg";
+import site64Img from "../img/64_card.jpeg";
 
 const SitesCard = ({ data }) => {
   return (
     <>
       <div className="md:w-[350px] w-full lg:w-[440px] rounded-xl flex-shrink-0 flex flex-col border-primary_light overflow-hidden shadow">
         <div className="h-60 overflow-hidden">
-          <img src={site1Img} className="h-80 object-cover w-full" />
+          <img src={data.id == "509QsdLlPh" ? site111Img : site64Img} className="h-80 object-cover w-full" />
         </div>
         <div className=" text-black py-2 px-2 mt-2">
           <div className="flex justify-between items-center">
