f87681ab13ea80edea9bcca05f63de195a0a21a3
Smit Patel <smitppatel35@gmail.com>
2024-10-20 12:29:35 +0530

    footer fixed

--- src/components/ConnectWithUs.jsx
+++ src/components/ConnectWithUs.jsx
@@ -143,15 +143,6 @@ const ConnectWithUs = (props) => {
           />
         </div>
       </div>
-      <div
-        className="flex absolute  -bottom-10 pt-5 left-1/2 -translate-x-1/2 flex-col items-center cursor-pointer "
-        onClick={() => scrollToBottom()}
-      >
-        <div className="rounded text-white bg-black bg-opacity-35 px-1 rounded overflow-hidden">
-          <MdKeyboardDoubleArrowDown className="text-2xl rounded  cursor-pointer animate-bounce" />
-        </div>
-        <p className="text-gray-400 text-sm font-semibold">Scroll Down</p>
-      </div>
     </section>
   );
 };
